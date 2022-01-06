e7b61f93fe39744c0adbd16b5432aa9d0948ebae
Grigori Grigoryan <grigori_grigoryan@epam.com>
2022-01-06 13:16:50 +0400

    Complete third task

--- index.js
+++ index.js
@@ -1,50 +1,24 @@
-//////////// Array Map ///////////////
-
-Array.prototype.myMap = function(cbf) {
-  var newArr = []
-  for (let i = 0; i < this.length; i++) {
-     newArr.push(cbf(this[i], i , this))
-  }
-  return newArr;
-}
-
-
-////////////////////////////////////////
-
-//////////// Array Slice ///////////////
-
-Array.prototype.mySlice = function (st, end) {
-  let newArr = [];
-  if (end < 0) end = this.length  - (-end);
-  if (st < 0) return  newArr;
-  for(st != end; st < end; st++ ) {
-    newArr.push(this[st])
-  }
-  return newArr
-}
-
-////////////////////////////////////////
-
-//////////// Array Flat ///////////////
-
-Array.prototype.myFlat = function (depth) { 
-  let newArr = [];
-  let list = this;
-  function flatting (arr, depth) {
-    for (let val of arr) {
-      if (!Array.isArray(val)) {
-        newArr.push(val);
-      } else {
-        if (depth >= 1) {
-          flatting(val, depth - 1)
-        } else {
-          newArr.push(val)
-        }
-      }
+const obj = {
+    name: "Jhon",
+    country: {
+     name: "Armenia",
+     code: 374
     }
-  }
-  flatting(list, depth);
-  return newArr;
+  };
+
+let reseveObject = {}
+
+  function printKeys(object, resObj) {
+    for (let key in object) {
+
+        if (typeof object[key] !== 'object' && !Array.isArray(object[key]) && object[key] !== null) {
+            resObj[object[key]] = key
+        }
+        if(typeof object[key] === 'object' ) {
+           resObj[key] =  printKeys(object[key], {})
+        }
+    }
+    return resObj;
 }
 
-////////////////////////////////////////
+ console.log( printKeys(obj, reseveObject)); 
